2d28ee2af8446155676d098042bd2345832f63a7
Wickramathilaka Arachchige Don Thilani Apsara Wickramathilaka <apsarathilani@gmail.com>
2025-08-30 07:54:46 +0530

    Update view_counting_page.dart

--- lib/screen/view_counting_page.dart
+++ lib/screen/view_counting_page.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-
+//viewcounting
 class OwnerCountingPage extends StatefulWidget {
   const OwnerCountingPage({Key? key}) : super(key: key);
 
